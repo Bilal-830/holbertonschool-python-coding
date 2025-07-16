ad43ff7f2116a791110ffb9082702a149baf1f0a
Bilal-830 <sayhanlalmahomed@gmail.com>
2025-07-16 10:25:04 +0400

    w

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,6 +1,21 @@
 #!/usr/bin/python3
+"""This module defines a Square class that represents a square."""
+
+
 class Square:
+    """Represents a square."""
+
     def __init__(self, size=0):
+        """
+        Initialize a new Square.
+
+        Args:
+            size (int): The size of the square (length of a side). Default is 0.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
